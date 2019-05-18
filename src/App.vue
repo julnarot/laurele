b2cfa0a32c9e6a6e1437f6bc60b63971d7f3ca72
Raul Jonatan <rauljhonatan.tola@gmail.com>
2019-05-18 01:53:16 -0500

    material

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,59 @@
 <template>
   <div id="app">
-  <li class="nav-item">
-  <router-link class="nav-link" to="/">Home</router-link>
-</li>
-<li class="nav-item">
-  <router-link class="nav-link" to="/blog">Blog</router-link>
-</li>
-<li class="nav-item">
-  <router-link class="nav-link" to="/services">Services</router-link>
- </li>
-<li class="nav-item">
-   <router-link class="nav-link" to="/contact">contact</router-link>
- </li>
+    <md-toolbar class="md-primary md-layout">
+      <div class="md-layout-item">
+        <li class="nav-item">
+          <router-link class="nav-link" to="/">Home</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/blog">Blog</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/services">Services</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/contact">contact</router-link>
+        </li>
+      </div>
+        <div class="md-layout-item">
+          <h3>LAURERLE</h3>
+        </div>
+        <div class="md-layout-item">
+          <div class="md-layout">
+            <div class="md-layout-item">
+              <div class="md-layout md-medium-size-50 md-small-size-50 md-xsmall-hide">
+                <div class="md-layout-item">
+                  <md-button class="">Default</md-button>
+                </div>
+                <div class="md-layout-item">
+                  <md-button class="md-raised md-accent">Acceder</md-button>
+                </div>
+              </div>
+            </div>
+            <div class="md-layout-item">
+              <md-menu md-size="big" md-direction="bottom-end">
+              <md-button class="md-icon-button" md-menu-trigger>
+                <md-icon>near_me</md-icon>
+              </md-button>
+              <md-menu-content>
+                <md-menu-item>
+                  <span>Find on map</span>
+                  <md-icon>near_me</md-icon>
+                </md-menu-item>
+                <md-menu-item>
+                  <span>Call</span>
+                  <md-icon>phone</md-icon>
+                </md-menu-item>
+              </md-menu-content>
+            </md-menu>
+            </div>
+          </div>
+        </div>
+    </md-toolbar>
     <router-view/>
+    <div class="footer">
+      <h1>footer</h1>
+    </div>
   </div>
 </template>
 <script>
@@ -23,15 +64,20 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /*margin-top: 60px;*/
+}
+.footer{
+  background: #2c3e50;
+  color: azure;
 }
 
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 ul {
--- src/components/home.vue
+++ src/components/home.vue
@@ -1,49 +1,72 @@
 <template>
-  <div class="row">
-      <div class="col-md-4 col-lg4" v-for="(data,index) in products" :key="index">
-        <img :src="data.image" class="img-fluid">
-         <h3 @click="goTodetail(data.productId)">{{data.productTitle}}</h3>
-      </div>
+  <div class="wrapper">
+  <input checked type=radio name="slider" id="slide1" />
+  <input type=radio name="slider" id="slide2" />
+  <input type=radio name="slider" id="slide3" />
+  <input type=radio name="slider" id="slide4" />
+  <input type=radio name="slider" id="slide5" />
+
+  <div class="slider-wrapper">
+    <div class=inner>
+      <article>
+        <div class="info top-left">
+          <h3>Malacca</h3></div>
+        <img src="https://farm9.staticflickr.com/8059/28286750501_dcc27b1332_h_d.jpg" />
+      </article>
+
+      <article>
+        <div class="info bottom-right">
+          <h3>Cameron Highland</h3></div>
+        <img src="https://farm6.staticflickr.com/5812/23394215774_b76cd33a87_h_d.jpg" />
+      </article>
+
+      <article>
+        <div class="info bottom-left">
+          <h3>New Delhi</h3></div>
+        <img src="https://farm8.staticflickr.com/7455/27879053992_ef3f41c4a0_h_d.jpg" />
+      </article>
+
+      <article>
+        <div class="info top-right">
+          <h3>Ladakh</h3></div>
+        <img src="https://farm8.staticflickr.com/7367/27980898905_72d106e501_h_d.jpg" />
+      </article>
+
+      <article>
+        <div class="info bottom-left">
+          <h3>Nubra Valley</h3></div>
+        <img src="https://farm8.staticflickr.com/7356/27980899895_9b6c394fec_h_d.jpg" />
+      </article>
     </div>
+    <!-- .inner -->
+  </div>
+  <!-- .slider-wrapper -->
+
+  <div class="slider-prev-next-control">
+    <label for=slide1></label>
+    <label for=slide2></label>
+    <label for=slide3></label>
+    <label for=slide4></label>
+    <label for=slide5></label>
+  </div>
+  <!-- .slider-prev-next-control -->
+
+  <div class="slider-dot-control">
+    <label for=slide1></label>
+    <label for=slide2></label>
+    <label for=slide3></label>
+    <label for=slide4></label>
+    <label for=slide5></label>
+  </div>
+  <!-- .slider-dot-control -->
+</div>
 </template>
 <script>
 export default {
   name: 'home',
   data () {
     return {
-      title: 'Home',
-      products: [
-        {
-          productTitle: 'ABCN',
-          image: require('../assets/logo.png'),
-          productId: 1
-        },
-        {
-          productTitle: 'KARMA',
-          image: require('../assets/logo.png'),
-          productId: 2
-        },
-        {
-          productTitle: 'Tino',
-          image: require('../assets/logo.png'),
-          productId: 3
-        },
-        {
-          productTitle: 'EFG',
-          image: require('../assets/logo.png'),
-          productId: 4
-        },
-        {
-          productTitle: 'MLI',
-          image: require('../assets/logo.png'),
-          productId: 5
-        },
-        {
-          productTitle: 'Banans',
-          image: require('../assets/logo.png'),
-          productId: 6
-        }
-      ]
+      title: 'Home'
     }
   },
   methods: {
@@ -56,4 +79,188 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 
 <style scoped>
+  .wrapper {
+  max-width: 100%;
+  margin: 1em auto;
+  position: relative;
+}
+
+input {
+  display: none;
+}
+
+.inner {
+  width: 300%;
+  line-height: 0;
+}
+
+article {
+  width: 20%;
+  float: left;
+  position: relative;
+}
+article img {
+  width: 100%;
+}
+
+/*---- SET UP CONTROL ----*/
+.slider-prev-next-control {
+  height: 50px;
+  position: absolute;
+  top: 50%;
+  width: 100%;
+}
+.slider-prev-next-control label {
+  display: none;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background: #fff;
+  opacity: 0.7;
+}
+.slider-prev-next-control label:hover {
+  opacity: 1;
+}
+
+.slider-dot-control {
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+  text-align: center;
+}
+.slider-dot-control label {
+  cursor: pointer;
+  border-radius: 5px;
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background: #bbb;
+}
+.slider-dot-control label:hover {
+  background: #ccc;
+  border-color: #777;
+}
+
+/* Info Box */
+.info {
+  position: absolute;
+  font-style: italic;
+  line-height: 20px;
+  opacity: 0;
+  color: #000;
+  text-align: left;
+}
+.info h3 {
+  color: #fcfff4;
+  margin: 0 0 5px;
+  font-weight: normal;
+  font-size: 1.5em;
+  font-style: normal;
+}
+.info.top-left {
+  top: 30px;
+  left: 30px;
+}
+.info.top-right {
+  top: 30px;
+  right: 30px;
+}
+.info.bottom-left {
+  bottom: 30px;
+  left: 30px;
+}
+.info.bottom-right {
+  bottom: 30px;
+  right: 30px;
+}
+
+/* Slider Styling */
+.slider-wrapper {
+  width: 100%;
+  overflow: hidden;
+  border-radius: 5px;
+  box-shadow: 1px 1px 4px #666;
+  background: #fff;
+  background: #fcfff4;
+}
+
+/*---- SET POSITION FOR SLIDE ----*/
+#slide1:checked ~ .slider-prev-next-control label:nth-child(5)::after, #slide5:checked ~ .slider-prev-next-control label:nth-child(4)::after, #slide4:checked ~ .slider-prev-next-control label:nth-child(3)::after, #slide3:checked ~ .slider-prev-next-control label:nth-child(2)::after, #slide2:checked ~ .slider-prev-next-control label:nth-child(1)::after, #slide5:checked ~ .slider-prev-next-control label:nth-child(1)::after, #slide4:checked ~ .slider-prev-next-control label:nth-child(5)::after, #slide3:checked ~ .slider-prev-next-control label:nth-child(4)::after, #slide2:checked ~ .slider-prev-next-control label:nth-child(3)::after, #slide1:checked ~ .slider-prev-next-control label:nth-child(2)::after {
+  font-family: FontAwesome;
+  font-style: normal;
+  font-weight: normal;
+  text-decoration: inherit;
+  margin: 0;
+  line-height: 38px;
+  font-size: 3em;
+  display: block;
+  color: #777;
+}
+
+#slide5:checked ~ .slider-prev-next-control label:nth-child(1)::after, #slide4:checked ~ .slider-prev-next-control label:nth-child(5)::after, #slide3:checked ~ .slider-prev-next-control label:nth-child(4)::after, #slide2:checked ~ .slider-prev-next-control label:nth-child(3)::after, #slide1:checked ~ .slider-prev-next-control label:nth-child(2)::after {
+  content: "";
+  padding-left: 15px;
+}
+
+#slide5:checked ~ .slider-prev-next-control label:nth-child(1), #slide4:checked ~ .slider-prev-next-control label:nth-child(5), #slide3:checked ~ .slider-prev-next-control label:nth-child(4), #slide2:checked ~ .slider-prev-next-control label:nth-child(3), #slide1:checked ~ .slider-prev-next-control label:nth-child(2) {
+  display: block;
+  float: right;
+  margin-right: 5px;
+}
+
+#slide1:checked ~ .slider-prev-next-control label:nth-child(5), #slide5:checked ~ .slider-prev-next-control label:nth-child(4), #slide4:checked ~ .slider-prev-next-control label:nth-child(3), #slide3:checked ~ .slider-prev-next-control label:nth-child(2), #slide2:checked ~ .slider-prev-next-control label:nth-child(1) {
+  display: block;
+  float: left;
+  margin-left: 5px;
+}
+
+#slide1:checked ~ .slider-prev-next-control label:nth-child(5)::after, #slide5:checked ~ .slider-prev-next-control label:nth-child(4)::after, #slide4:checked ~ .slider-prev-next-control label:nth-child(3)::after, #slide3:checked ~ .slider-prev-next-control label:nth-child(2)::after, #slide2:checked ~ .slider-prev-next-control label:nth-child(1)::after {
+  content: "";
+  padding-left: 8px;
+}
+
+#slide5:checked ~ .slider-dot-control label:nth-child(5), #slide4:checked ~ .slider-dot-control label:nth-child(4), #slide3:checked ~ .slider-dot-control label:nth-child(3), #slide2:checked ~ .slider-dot-control label:nth-child(2), #slide1:checked ~ .slider-dot-control label:nth-child(1) {
+  background: #333;
+}
+
+#slide5:checked ~ .slider-wrapper article:nth-child(5) .info, #slide4:checked ~ .slider-wrapper article:nth-child(4) .info, #slide3:checked ~ .slider-wrapper article:nth-child(3) .info, #slide2:checked ~ .slider-wrapper article:nth-child(2) .info, #slide1:checked ~ .slider-wrapper article:nth-child(1) .info {
+  opacity: 1;
+}
+
+#slide1:checked ~ .slider-wrapper .inner {
+  margin-left: 0%;
+}
+#slide2:checked ~ .slider-wrapper .inner {
+  margin-left: -100%;
+}
+#slide3:checked ~ .slider-wrapper .inner {
+  margin-left: -200%;
+}
+#slide4:checked ~ .slider-wrapper .inner {
+  margin-left: -300%;
+}
+#slide5:checked ~ .slider-wrapper .inner {
+  margin-left: -400%;
+}
+/*---- TABLET ----*/
+@media only screen and (max-width: 850px) and (min-width: 450px) {
+  .slider-wrapper {
+    border-radius: 0;
+  }
+}
+/*---- MOBILE----*/
+@media only screen and (max-width: 450px) {
+  .slider-wrapper {
+    border-radius: 0;
+  }
+
+  .slider-wrapper .info {
+    opacity: 0;
+  }
+}
+@media only screen and (min-width: 850px) {
+  body {
+    padding: 0 80px;
+  }
+}
 </style>
