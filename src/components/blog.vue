a9a86b41f83db6d2a2c1884b7a5879a17f8f1feb
Raul Jonatan <rauljhonatan.tola@gmail.com>
2019-05-27 02:17:47 -0500

    implementacion de vuex

--- src/components/blog.vue
+++ src/components/blog.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="blog">
     <h1>Blog{{msg}}</h1>
+    <h4><b>Contador{{contador}}</b></h4>
+    <button type="submit" @click="increment()">incrementar</button>
+    <button type="submit" @click="decrement()">-</button>
+    <!--input @input="increment" name="flavor"-->
+    <input v-model="numero" v-bind:placeholder="placeholder">
+    <br>
+    <div v-if="onSession">
+      <input  type="text" v-model="username" placeholder="usuario">
+      <input type="password" v-model="password" placeholder="password">
+      <md-button class="md-raised md-accent" @click="login()">Login</md-button>
+    </div>
   </div>
 </template>
 <script>
@@ -8,7 +19,59 @@ export default {
   name: 'blog',
   data () {
     return {
-      msg: 'Blog!!'
+      msg: 'Blog!!',
+      numero: '',
+      placeholder: 'Número',
+      username: '',
+      password: ''
+      // onSession: false,
+    }
+  },
+  created: function () {
+    // this.isLoged()
+  },
+  methods: {
+    saludo () {
+      console.log('hola mundo', this.$store)
+    },
+    decrement () {
+      this.$store.commit('decrement')
+    },
+    increment () {
+      const num = Number(this.numero)
+      // if (typeof(num)==='number' && !isNaN(num)) {
+      if (num) {
+        this.$store.commit('incrementByValue', Number(this.numero))
+      } else {
+        setTimeout(() => {
+          this.numero = ''
+          this.placeholder = ':/ Número inválido'
+        }, 0)
+        setTimeout(() => {
+          this.placeholder = 'Número'
+        }, 2000)
+      }
+    },
+    login () {
+      this.$store.dispatch('obtainToken', {username: this.username, password: this.password})
+        .then(response => {
+          // console.log(response.data) // shows the user data good stuff
+          console.log('Got some user data, now lets show something with it', response)
+        }, () => {
+          // console.log(error.response) // shows the API error invalid credentials type junk
+          console.log('Got nothing for this user except bad news.')
+        })
+    }
+  },
+  isLoged: function () {
+    this.onSession = this.$store.getters.isLoged
+  },
+  computed: {
+    contador: function () {
+      return this.$store.getters.obtenerContador
+    },
+    onSession: function () {
+      return !this.$store.getters.isLoged
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -4,12 +4,106 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import * as JWT from 'jwt-decode'
+
+import Vuex from 'vuex'
+Vue.use(Vuex)
+Vue.use(VueAxios)
+Vue.use(axios)
+
 Vue.config.productionTip = false
+// const urlApi = 'http://localhost:8000/'
+const store = new Vuex.Store({
+  state: {
+    jwt: localStorage.getItem('t'),
+    endpoints: {
+      obtainJWT: 'https://jcmsresource.herokuapp.com/auth/obtain_token/',
+      refreshJWT: 'https://jcmsresource.herokuapp.com/auth/refresh_token'
+    },
+    contador: 1
+  },
+
+  mutations: {
+    updateToken (state, newToken) {
+      localStorage.setItem('t', newToken)
+      state.jwt = newToken
+    },
+    removeToken (state) {
+      localStorage.removeItem('t')
+      state.jwt = null
+    },
+    incrementByValue (state, valor) {
+      state.contador += valor
+    },
+    decrement (state) {
+      state.contador = state.contador - 1
+    }
+  },
+  actions: {
+    // WE WILL ADD THIS LATER
+    obtainToken (context, {username, password}) {
+      const payload = {
+        username: username,
+        password: password
+      }
+      axios.post(this.state.endpoints.obtainJWT, payload)
+        .then((response) => {
+          this.commit('updateToken', response.data.token)
+        })
+        .catch((error) => {
+          console.log(error, store)
+        })
+    },
+    refreshToken () {
+      const payload = {
+        token: this.state.jwt
+      }
+      axios.post(this.state.endpoints.refreshJWT, payload)
+        .then((response) => {
+          this.commit('updateToken', response.data.token)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    },
+    inspectToken () {
+      const token = this.state.jwt
+      if (token) {
+        const decoded = JWT(token)
+        const exp = decoded.exp
+        const origIat = decoded.orig_iat
+        if (exp - (Date.now() / 1000) < 1800 && (Date.now() / 1000) - origIat < 628200) {
+          this.dispatch('refreshToken')
+        } else if (exp - (Date.now() / 1000) < 1800) {
+          // DO NOTHING, DO NOT REFRESH
+          console.log()
+        } else {
+          // PROMPT USER TO RE-LOGIN, THIS ELSE CLAUSE COVERS THE CONDITION WHERE A TOKEN IS EXPIRED AS WELL
+        }
+      }
+    }
+  },
+  getters: {
+    obtenerContador: function (state) {
+      return `Es: ${state.contador}`
+    },
+    isLoged: function (state) {
+      if (state.jwt) {
+        return true
+      } else {
+        return false
+      }
+    }
+  }
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
