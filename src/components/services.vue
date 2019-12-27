ace759ca39da5cd39976bbd65f0827dfaa20fe22
rjotac <rauljhonatan.tola@gmail.com>
2019-12-27 04:30:38 +0000

    restores
    restores

--- src/components/services.vue
+++ src/components/services.vue
@@ -17,25 +17,6 @@
               </tr>
             </tbody>
     </table>
-    <md-card v-for="(data,index) in repositories" :key="index">
-      <md-card-media-cover>
-        <md-card-media md-ratio="16:9" v-if="data.owner">
-          <img v-bind:src="{{ data.owner.avatar_url }}" alt="Skyscraper">
-        </md-card-media>
-
-        <md-card-area>
-          <md-card-header>
-            <span class="md-title">{{ data.name }}</span>
-            <span class="md-subhead">{{ data.id }}</span>
-          </md-card-header>
-
-          <md-card-actions>
-            <md-button>Neither this</md-button>
-            <md-button>Or this</md-button>
-          </md-card-actions>
-        </md-card-area>
-      </md-card-media-cover>
-    </md-card>
     <md-progress-spinner v-if="repositoriesLoader"
     :md-diameter="100" :md-stroke="10" md-mode="indeterminate"></md-progress-spinner>
   </div>
